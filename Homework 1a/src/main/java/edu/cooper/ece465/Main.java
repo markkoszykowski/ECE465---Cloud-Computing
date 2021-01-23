822f6b80a37f1093bb3d953668aee71365c5d8ba
Mark Koszykowski <markkoszykowski@gmail.com>
2021-01-23 15:04:46 -0500

    test

---
+++ Homework 1a/src/main/java/edu/cooper/ece465/Main.java
@@ -0,0 +1,8 @@
+package main.java.edu.cooper.ece465;
+
+public class Main {
+    public static void main(String args[]) {
+        Dijsktra d = new Dijkstra();
+        System.out.println("Hello, World");
+    }
+}

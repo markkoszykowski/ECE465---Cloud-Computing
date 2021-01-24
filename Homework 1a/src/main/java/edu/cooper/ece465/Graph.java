3144c08847e0adaba38af55e6dbdc6da56b46029
Mark Koszykowski <markkoszykowski@gmail.com>
2021-01-24 18:39:20 -0500

    added functionality

--- Homework 1a/src/main/java/edu/cooper/ece465/Graph.java
+++ Homework 1a/src/main/java/edu/cooper/ece465/Graph.java
@@ -1,8 +1,112 @@
 package edu.cooper.ece465;
 
+import java.io.*;
+import java.util.*;
+
 public class Graph {
-    public Graph() {
-        System.out.println("Created!");
+
+    private class Node {
+        String name;
+        int dist;
+        boolean known;
+        List<Edge> adjList;
+        List<String> shortPath;
+
+        Node() {
+            adjList = new ArrayList<Edge>();
+            shortPath = new ArrayList<String>();
+        }
     }
-    public void dijkstra() { System.out.println("Doing something?");}
+
+    private class Edge {
+        Node dest;
+        int cost;
+    }
+
+    private List<Node> nodeList;
+    private Hashtable<String, Node> encountered;
+
+    public Graph() {
+        nodeList = new ArrayList<Node>();
+        encountered = new Hashtable<String, Node>();
+    }
+
+    public void makeGraph(String inFile) throws Exception {
+        File graphFile = new File(inFile);
+        BufferedReader br = new BufferedReader(new FileReader(graphFile));
+
+        String begin, end, cost;
+        Node beginNode, endNode;
+        while((begin = br.readLine()) != null && (end = br.readLine()) != null && (cost = br.readLine()) != null) {
+            Node temp1 = new Node();
+            if(!encountered.containsKey(begin)) {
+                temp1.name = begin;
+                temp1.dist = Integer.MAX_VALUE;
+                temp1.known = false;
+
+                nodeList.add(temp1);
+                encountered.put(begin, temp1);
+            }
+            beginNode = encountered.get(begin);
+
+            Node temp2 = new Node();
+            if(!encountered.containsKey(end)) {
+                temp2.name = end;
+                temp2.dist = Integer.MAX_VALUE;
+                temp2.known = false;
+
+                nodeList.add(temp2);
+                encountered.put(end, temp2);
+            }
+            endNode = encountered.get(end);
+
+            Edge e = new Edge();
+            e.dest = endNode;
+            e.cost = Integer.parseInt(cost);
+            beginNode.adjList.add(e);
+        }
+        br.close();
+    }
+
+    public void printGraph() {
+        for(Node n : nodeList) {
+            System.out.println("Origin Node Name: " + n.name);
+            for(Edge e : n.adjList) {
+                System.out.println("\tDestination Node Name: " + e.dest.name);
+
+                System.out.println("\tCost: " + e.cost + "\n");
+            }
+            if(n.adjList.isEmpty()) { System.out.println("\tNo Destinations\n"); }
+        }
+    }
+
+    public void dijkstra(String vertex) {
+        System.out.println("Doing something?");
+    }
+
+    public boolean checkHash(String vertex) {
+        return encountered.containsKey(vertex);
+    }
+
+    public void makeOut(String outFile) throws Exception {
+        PrintWriter shortFile = new PrintWriter(outFile, "UTF-8");
+        for(Node n : nodeList) {
+            shortFile.printf("%s: ", n.name);
+
+            if(n.shortPath.isEmpty()) {
+                shortFile.println("NO PATH");
+            }
+            else {
+                shortFile.printf(" [");
+                for(String s : n.shortPath) {
+                    shortFile.printf("%s", s);
+                    if(!n.shortPath.get(n.shortPath.size() - 1).equals(s)) {
+                        shortFile.printf(", ");
+                    }
+                }
+                shortFile.println("]");
+            }
+        }
+        shortFile.close();
+    }
 }
--- Homework 1a/src/main/java/edu/cooper/ece465/Main.java
+++ Homework 1a/src/main/java/edu/cooper/ece465/Main.java
@@ -1,9 +1,31 @@
 package edu.cooper.ece465;
 
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws Exception {
+        String inFile, outFile, vertex;
         Graph g = new Graph();
-        System.out.println("Hello, World");
-        g.dijkstra();
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter name of graph file: ");
+        inFile = in.nextLine();
+        g.makeGraph(inFile);
+
+        g.printGraph();
+
+        do {
+            System.out.println("Enter a valid vertex name for the starting vertex: ");
+            vertex = in.nextLine();
+        } while(!g.checkHash(vertex));
+
+        g.dijkstra(vertex);
+
+        System.out.println("Enter name of output file: ");
+        outFile = in.nextLine();
+        g.makeOut(outFile);
+
+        return;
     }
 }

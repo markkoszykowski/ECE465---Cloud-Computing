9fcb8c8df935c2bec8b6edda527827f720158b96
Mark Koszykowski <markkoszykowski@gmail.com>
2021-05-09 22:36:18 -0400

    removed nonsense

--- Final Project/src/main/java/edu/cooper/ece465/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package edu.cooper.ece465;
-
-public class HelloWorld {
-    public static void main(String[] args) throws InterruptedException {
-        Thread.sleep(30*1000);
-        System.out.println("hello world!");
-    }
-}
--- Final Project/src/main/java/edu/cooper/ece465/test.java
+++
@@ -1,7 +0,0 @@
-package edu.cooper.ece465;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println("Working Directory = " + System.getProperty("user.dir"));
-    }
-}
